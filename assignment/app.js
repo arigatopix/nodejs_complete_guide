9b05ddca3901f990f5bf4ba19d45fa5571665e0f
arigato <teeruch.k@gmail.com>
2019-05-23 22:50:00 +0700

    :fire: solved

--- assignment/app.js
+++ assignment/app.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
 
 // ต้องเอา path อื่นที่ไม่ใช่ '/' ไว้อันแรก เพราะว่า middleware จะไม่ทำอันอื่น และ code จะอ่านจากบนไปล่าง ยกเว้นจะมี next(); ไว้ด้านใน
+
+// '/users' เรียกว่า filter และจัดการด้วย middleware อย่าลืมต้องเรียงลำดับให้ถูก
 app.use('/users', (req, res, next) => {
   console.log('From Users');
   res.send('<h1>Hello from Users Page</h1>');
