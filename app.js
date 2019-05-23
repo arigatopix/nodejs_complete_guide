a0fc8f61c671e6eae69aaf80b3be2cd5cd0f0739
arigato <teeruch.k@gmail.com>
2019-05-23 22:23:16 +0700

    Handling Difference route

--- app.js
+++ app.js
@@ -5,16 +5,20 @@ const app = express();
 
 // express docs
 // app.use(path, callback) เพิ่ม path เข้าไป เป็น relative path
-app.use('/add-product',(req, res, next) => {
-  console.log('In another middleware');
-  // ไม่ต้องส่ง content-type เอง express ช่วยทำให้ clean code มากขึ้น
-  res.send('<h1>The "Add Product" Page</h1>')
+app.use('/', (req, res, next) => {
+  console.log('Alway run');
+  next();
 });
 
-app.use('/',(req, res, next) => {
+app.use('/add-product', (req, res, next) => {
   console.log('In another middleware');
   // ไม่ต้องส่ง content-type เอง express ช่วยทำให้ clean code มากขึ้น
-  res.send('<h1>Hello from Express.js</h1>')
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  // ไม่ต้องส่ง content-type เอง express ช่วยทำให้ clean code มากขึ้น
+  res.send('<h1>Hello from Express.js</h1>');
 });
 
 // * listen server ใช้ได้เพราะ express serve function ให้
