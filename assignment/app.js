0b29d33c011f2c01f7943fc167fe813b7508f296
arigato <teeruch.k@gmail.com>
2019-05-23 22:44:17 +0700

    :fire: Express.js assignment

---
+++ assignment/app.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+// ต้องเอา path อื่นที่ไม่ใช่ '/' ไว้อันแรก เพราะว่า middleware จะไม่ทำอันอื่น และ code จะอ่านจากบนไปล่าง ยกเว้นจะมี next(); ไว้ด้านใน
+app.use('/users', (req, res, next) => {
+  console.log('From Users');
+  res.send('<h1>Hello from Users Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('Hello world');
+  res.send('<h1>Hello from "INDEX"</h1>');
+});
+
+app.listen(3001);
